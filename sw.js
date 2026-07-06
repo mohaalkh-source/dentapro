28fa7c7b5b86aeafda792ac75e1679c5bf5f90a4
mohaalkh-source <moh.a.alkh@gmail.com>
2026-07-07 02:01:30 +0300

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'dentapro-cache-v1';
+const urlsToCache = [
+  '/dentapro/',
+  '/dentapro/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
