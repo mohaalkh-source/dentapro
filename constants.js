f23f813ae1ebe1fe2db9e3d3fc86dc1398e332a7
mohaalkh-source <moh.a.alkh@gmail.com>
2026-08-21 14:10:56 +0300

    Delete constants.js

--- constants.js
+++
@@ -1,3 +0,0 @@
-export const APP_NAME = 'DentaPro';
-export const SUPPORTED_LANGUAGES = Object.freeze(['ar', 'en']);
-export const STORAGE_KEYS = Object.freeze({ cart: 'dentapro_cart', products: 'dentapro_products', session: 'dentapro_session' });
