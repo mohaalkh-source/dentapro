88c8133d5d881b6b43bf0c1d26978cc9e27ad2cd
mohaalkh-source <moh.a.alkh@gmail.com>
2026-08-21 17:16:48 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,1 @@
+export const APP_CONFIG = Object.freeze({ name: 'DentaPro', defaultLanguage: 'ar', currency: 'د.أ' });
---
+++ js/constants.js
@@ -0,0 +1,3 @@
+export const APP_NAME = 'DentaPro';
+export const SUPPORTED_LANGUAGES = Object.freeze(['ar', 'en']);
+export const STORAGE_KEYS = Object.freeze({ cart: 'dentapro_cart', products: 'dentapro_products', session: 'dentapro_session' });
