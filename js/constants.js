accdb23c26268af8304fd405258bf499f5c7ae40
mohaalkh-source <moh.a.alkh@gmail.com>
2026-09-14 02:24:02 +0300

    Add files via upload

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,7 @@
 export const APP_NAME = 'DentaPro';
 export const SUPPORTED_LANGUAGES = Object.freeze(['ar', 'en']);
 export const STORAGE_KEYS = Object.freeze({ cart: 'dentapro_cart', products: 'dentapro_products', session: 'dentapro_session' });
+
+// عند تفعيلها (بعد نشر Cloud Function وترقية Blaze)، إنشاء الطلبات (Quick Order وسلة الشراء)
+// ينتقل من الحساب المحلي في المتصفح إلى دالة createOrder على الخادم
+window.SERVER_ORDER_CREATION_ENABLED = false;
